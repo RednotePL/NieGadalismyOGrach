00b08eef54cc84b1d495e435d6f40e6dc82adeaa
SuperStarPL <f1time@o2.pl>
2019-10-20 14:43:58 +0200

    Interfejs GameStage

---
+++ NieGadalismyOGrach/Interfaces/IGameStage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NieGadalismyOGrach.Interfaces
+{
+    internal interface IGameStage
+    {
+        string StageName { get; set; }
+        Action OnStageWin { get; set; }
+        Action OnStageLose { get; set; }
+
+        void OnStageStart();
+
+        void OnStageEnd();
+    }
+}
